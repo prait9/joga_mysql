22cbd633dc53a774eb4493a3062d8034ffcc9b12
Prait Kirilo <prait.kirilo2@gmail.com>
2026-01-21 13:45:40 +0200

    Created article controller and route, refactored application code

--- index.js
+++ index.js
@@ -1,63 +1,30 @@
-const express = require("express");
-const path = require("path");
-const mysql = require("mysql2");
-const { engine } = require("express-handlebars");
+const Express = require("express")
+const path = require('path')
+const mysql = require('mysql2')
+const hbs = require('express-handlebars')
+const app = Express()
+const bodyParser = require('body-parser')
 
-const app = express();
-
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.engine(
-  "hbs",
-  engine({
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "hbs")
+app.engine("hbs", hbs.engine({
     extname: "hbs",
     defaultLayout: "main",
-    layoutsDir: path.join(__dirname, "views", "layouts"),
-  })
-);
+    layoutsDir: __dirname + "/views/layouts/"
+}))
+app.use(Express.static("public"))
 
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(Express.urlencoded({extended: true}))
+
+const articleRoutes = require('./routes/article.js')
+
+app.use('/', articleRoutes)
 
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "qwerty", 
-  database: "joga_mysql",  
-});
+const authorRoutes = require('./routes/author.js')
+app.use('/author', authorRoutes);
 
-con.connect((err) => {
-  if (err) {
-    console.error(" MySQL connect error:", err.message);
-    process.exit(1);
-  }
-  console.log(" Connected to MySQL");
-});
-
-
-app.get("/", (req, res) => {
-  const query = "SELECT * FROM article";
-  con.query(query, (err, result) => {
-    if (err) return res.status(500).send(err.message);
-    res.render("index", { articles: result });
-  });
-});
-
-
-app.get("/article/:slug", (req, res) => {
-  const query = "SELECT * FROM article WHERE slug = ? LIMIT 1";
-  con.query(query, [req.params.slug], (err, result) => {
-    if (err) return res.status(500).send(err.message);
-    if (!result.length) return res.status(404).send("Article not found");
-
-    res.render("article", { article: result[0] });
-  });
-});
-
-
-app.listen(3000, () => {
-  console.log("App is started at http://localhost:3000");
-});
+app.listen(3001, () =>{
+    console.log('app is at http://localhost:3001')
+}) 
