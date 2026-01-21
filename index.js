5f3d1597e5c5f96bea4dd3e79c62eae32bd6f0a5
Prait Kirilo <prait.kirilo2@gmail.com>
2026-01-21 13:13:50 +0200

    Displayed an article by slug

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
-const express = require('express');
-const path = require('path');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const hbs = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql2");
+const { engine } = require("express-handlebars");
 
 const app = express();
 
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+  })
+);
+
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('hbs', hbs.engine({
-  extname: 'hbs',
-  defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, 'views', 'layouts'),
-}));
-app.set('view engine', 'hbs');
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'qwerty',
-  database: 'joga_mysql',
+  host: "localhost",
+  user: "root",
+  password: "qwerty", 
+  database: "joga_mysql",  
 });
 
 con.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to joga_mysql db');
+  if (err) {
+    console.error(" MySQL connect error:", err.message);
+    process.exit(1);
+  }
+  console.log(" Connected to MySQL");
 });
 
-app.listen(3003, () => {
-  console.log('App is started at http://localhost:3003');
+
+app.get("/", (req, res) => {
+  const query = "SELECT * FROM article";
+  con.query(query, (err, result) => {
+    if (err) return res.status(500).send(err.message);
+    res.render("index", { articles: result });
+  });
+});
+
+
+app.get("/article/:slug", (req, res) => {
+  const query = "SELECT * FROM article WHERE slug = ? LIMIT 1";
+  con.query(query, [req.params.slug], (err, result) => {
+    if (err) return res.status(500).send(err.message);
+    if (!result.length) return res.status(404).send("Article not found");
+
+    res.render("article", { article: result[0] });
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log("App is started at http://localhost:3000");
 });
